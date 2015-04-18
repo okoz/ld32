8368035cffb524eab6a0d0739dd1228e2efde97a
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2015-04-18 12:39:36 -0400

    Working on animals

---
+++ Assets/TargetPanningCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetPanningCamera : MonoBehaviour
+{
+    public Transform Target;
+    public float PanSpeed;
+
+    private float panAmount;
+
+	void Start()
+    {
+	
+	}
+	
+	void Update()
+    {
+        panAmount = Input.GetAxis("Horizontal");
+	}
+
+    void LateUpdate()
+    {
+        transform.LookAt(Target);
+
+        if(Target)
+        {
+            transform.RotateAround(Target.transform.position, Vector3.up, PanSpeed * panAmount);
+        }
+    }
+}
