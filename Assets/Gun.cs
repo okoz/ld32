4f3c6d571d5dada054c8a858d0c979d1e6e8cdc2
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2015-04-18 21:43:59 -0400

    Temporary gun mesh

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -25,19 +25,21 @@ public class Gun : MonoBehaviour
 	
 	void Update()
     {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        AimAt(mouseRay.direction);
+
         if (Input.GetButtonUp("Fire1"))
         {
             if (Projectiles.Length > 0)
             {
                 GameObject projectile = GameObject.Instantiate<GameObject>(Projectiles[0]);
 
-                Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                projectile.transform.position = mouseRay.origin;
+                projectile.transform.position = transform.position;
 
                 Rigidbody rb = projectile.GetComponent<Rigidbody>();
                 if (rb != null)
                 {
-                    rb.AddForce(mouseRay.direction * GetPower(), ForceMode.Impulse);
+                    rb.AddForce(transform.forward * GetPower(), ForceMode.Impulse);
                 }
             }
 
@@ -71,17 +73,21 @@ public class Gun : MonoBehaviour
     {
         if (Projectiles.Length > 0)
         {
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             float mass = Projectiles[0].GetComponent<Rigidbody>().mass;
-            Vector3 v0 = mouseRay.direction * power / mass;
+            Vector3 v0 = transform.forward * power / mass;
 
             lineRenderer.SetVertexCount(32);
             for(int i = 0; i < 32; ++i)
             {
                 float t = 0.1f * i;
-                Vector3 p = mouseRay.origin + v0 * t + 0.5f * Physics.gravity * t * t;
+                Vector3 p = transform.position + v0 * t + 0.5f * Physics.gravity * t * t;
                 lineRenderer.SetPosition(i, p);
             }
         }
+    }
+
+    private void AimAt(Vector3 direction)
+    {
+        transform.LookAt(transform.position + direction, Vector3.up);
     }
 }
