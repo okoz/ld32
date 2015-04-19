7406df25d92cc06022621a211b07603be42789d1
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2015-04-19 19:05:55 -0400

    Working on new projectiles, prancing sheep

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,4 +4,19 @@ using System.Collections;
 public class Bullet : MonoBehaviour
 {
     public Sprite Icon;
+
+    private Rigidbody rigidBody;
+
+    public void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void Update()
+    {
+        if (rigidBody.velocity.sqrMagnitude > 0.01f)
+        {
+            transform.LookAt(transform.position + rigidBody.velocity);
+        }
+    }
 }
