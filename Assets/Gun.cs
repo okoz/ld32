5aac2deb3c38ca9e25ba4c48a8786e60af238337
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2015-04-20 02:11:53 -0400

    Simple trajectories

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -9,65 +9,55 @@ public class Gun : MonoBehaviour
     public float MinForce;
     public Slider FireStatus;
     public Image FireStatusBackground;
-    public Image ActiveWeaponIcon;
+    public GameObject[] GunPoints;
 
     private LineRenderer lineRenderer;
-    private GameObject activeProjectile;
+    private Plane groundPlane = new Plane(Vector3.up, 0.0f);
 
 	void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        SetProjectile(0);
 	}
 	
 	void Update()
     {
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        AimAt(mouseRay.direction);
+        float t;
+        groundPlane.Raycast(mouseRay, out t);
+        Vector3 target = mouseRay.GetPoint(t);
+        Vector3 origin = GetNearestGunPoint(target).transform.position;
 
-        if (Input.GetButtonDown("Weapon 1"))
-        {
-            SetProjectile(0);
-        }
-        else if(Input.GetButtonDown("Weapon 2"))
-        {
-            SetProjectile(1);
-        }
+        float dx = (origin.ProjectY(0) - target.ProjectY(0)).magnitude;
+        float dy = -origin.y;
+        float v0 = dx * Mathf.Sqrt(-Physics.gravity.magnitude / (2.0f * dy));
+
+        DisplayFireArc(origin, target);
 
         if (Input.GetButtonUp("Fire1"))
         {
-            if (activeProjectile != null)
+            GameObject projectile = GameObject.Instantiate<GameObject>(Projectiles[0]);
+
+            projectile.transform.position = origin;
+
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            if (rb != null)
             {
-                GameObject projectile = GameObject.Instantiate<GameObject>(activeProjectile);
-
-                projectile.transform.position = transform.position;
-
-                Rigidbody rb = projectile.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    rb.AddForce(transform.forward * GetPower(), ForceMode.Impulse);
-                }
+                rb.AddForce((target - origin).ProjectY(0).normalized * v0, ForceMode.Impulse);
             }
-
-            SetSliderValue(0.0f);
         }
-        else if (Input.GetButtonDown("Fire1"))
+        else if (Input.GetButtonUp("Fire2"))
         {
-            SetSliderValue(0.0f);
-        }
+            GameObject projectile = GameObject.Instantiate<GameObject>(Projectiles[1]);
 
-        if (Input.GetButton("Fire1"))
-        {
-            SetSliderValue(FireStatus.value + Time.deltaTime);
-        }
+            projectile.transform.position = origin;
 
-        DisplayFireArc(GetPower());
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce((target - origin).ProjectY(0).normalized * v0, ForceMode.Impulse);
+            }
+        }
 	}
-
-    private float GetPower()
-    {
-        return MinForce + FireStatus.value * (MaxForce - MinForce);
-    }
 
     private void SetSliderValue(float value)
     {
@@ -75,37 +65,34 @@ public class Gun : MonoBehaviour
         FireStatusBackground.color = ExtensionMethods.FromHSV(FireStatus.value * 100.0f, 1.0f, 1.0f);
     }
 
-    private void DisplayFireArc(float power)
+    private void DisplayFireArc(Vector3 origin, Vector3 target)
     {
-        if (Projectiles.Length > 0)
-        {
-            float mass = Projectiles[0].GetComponent<Rigidbody>().mass;
-            Vector3 v0 = transform.forward * power / mass;
+        float dx = (origin.ProjectY(0) - target.ProjectY(0)).magnitude;
+        float dy = -origin.y;
+        float v0 = dx * Mathf.Sqrt(-Physics.gravity.magnitude / (2.0f * dy));
 
-            lineRenderer.SetVertexCount(32);
-            for(int i = 0; i < 32; ++i)
+        lineRenderer.SetVertexCount(32);
+        for(int i = 0; i < 32; ++i)
+        {
+            float t = 0.1f * i;
+            Vector3 p = origin + v0 * (target - origin).ProjectY(0).normalized * t + 0.5f * Physics.gravity * t * t;
+            lineRenderer.SetPosition(i, p);
+        }
+    }
+
+    private GameObject GetNearestGunPoint(Vector3 target)
+    {
+        float distance = float.MaxValue;
+        GameObject nearest = null;
+        foreach (GameObject gunPoint in GunPoints)
+        {
+            float d = (gunPoint.transform.position - target).sqrMagnitude;
+            if (d < distance)
             {
-                float t = 0.1f * i;
-                Vector3 p = transform.position + v0 * t + 0.5f * Physics.gravity * t * t;
-                lineRenderer.SetPosition(i, p);
+                nearest = gunPoint;
+                distance = d;
             }
         }
-    }
-
-    private void AimAt(Vector3 direction)
-    {
-        transform.LookAt(transform.position + direction, Vector3.up);
-    }
-
-    private void SetProjectile(int i)
-    {
-        if (i < Projectiles.Length)
-        {
-            activeProjectile = Projectiles[i];
-
-            Bullet bullet = activeProjectile.GetComponent<Bullet>();
-            ActiveWeaponIcon.sprite = bullet.Icon;
-            
-        }
+        return nearest;
     }
 }
