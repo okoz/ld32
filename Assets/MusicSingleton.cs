f9b378fd60dd7b68c6d39bd344c60c545544feb7
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2015-04-20 20:46:37 -0400

    Music! It doesn't work yet

---
+++ Assets/MusicSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSingleton : MonoBehaviour {
+
+    private static MusicSingleton instance = null;
+
+	void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
